aa93c701d92ae1045cdc2ce9fd882e45be1a91f9
Zhak-Petrovich <denka.dyachenko@gmail.com>
2023-03-28 14:17:08 +0300

    Initial commit

---
+++ src/main/java/com/adl/adl_v2/AdlV2Application.java
@@ -0,0 +1,13 @@
+package com.adl.adl_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdlV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdlV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/adl/adl_v2/AdlV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adl.adl_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdlV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
